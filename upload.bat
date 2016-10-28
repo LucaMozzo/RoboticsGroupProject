6463dce111275dc83c981846cc1addaf186310ab
asif5 <k1502710@kcl.ac.uk>
2016-10-28 10:37:15 +0100

    Merge branch 'master' of D:\Users\asif5\IdeaProjects\RoboticsGroupProject with conflicts.

--- upload.bat
+++ upload.bat
@@ -1,2 +1,2 @@
-copy "C:\Users\Artur\OneDrive\School\Kings Undergraduate\RGB\RoboticsGroupProject\out\artifacts\RoboticsGroupProject_jar\RoboticsGroupProject.jar" RoboticsGroupProject.jar
+copy "D:\Users\asif5\IdeaProjects\RoboticsGroupProject\out\artifacts\RoboticsGroupProject_jar\RoboticsGroupProject.jar" RoboticsGroupProject.jar
 pscp -scp RoboticsGroupProject.jar root@10.0.1.1:/home/lejos/programs
