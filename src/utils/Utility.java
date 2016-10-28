eed56bc90fd90d3e145b134804a18167507dfb55
Luca Mozzo <lucamozzo21173@gmail.com>
2016-10-28 10:45:50 +0100

    added displayString method

--- src/utils/Utility.java
+++ src/utils/Utility.java
@@ -1,7 +1,15 @@
 package utils;
 
+import lejos.hardware.lcd.LCD;
+
 /**
  * Created by Artur on 26-Oct-16.
  */
-public class Utility {
+public final class Utility {
+  
+  public static void displayString(String str){
+    LCD.clear();
+    LCD.drawString(str,1,1);
+    LCD.refresh();
+  }
 }
